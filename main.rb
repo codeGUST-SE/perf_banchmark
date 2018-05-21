b90d8ea6fca895e58d35b9136ec07de2d75d84e7
evis-go <evis6174@gmail.com>
2018-05-22 00:10:57 +0300

    Implemented the random query generation part for benchmark_random_queries experiment

--- main.rb
+++ main.rb
@@ -1,9 +1,19 @@
 require 'open-uri'
 require 'nokogiri'
+require 'set'
 
 URL = "https://codegust.appspot.com/search?utf8=%E2%9C%93&q="
 POLITENESS_POLICY_WAIT = 0
 TRIES = 3
+TEST_PER_QUERY = 500
+
+def rand_n(n, max)
+  randoms = Set.new
+  loop do
+    randoms << rand(max-1)
+    return randoms.to_a if randoms.size >= n
+  end
+end
 
 def benchmark_nerd_terminologies
   results = {1=>[], 2=>[], 3=>[], 4=>[], 5=>[]}
@@ -32,4 +42,31 @@ def benchmark_nerd_terminologies
   end
 end
 
-benchmark_nerd_terminologies
+def benchmark_random_queries
+  results = { 1=>[], 2=>[], 3=>[], 4=>[], 5=>[] }
+  words = Set.new
+  File.open("nerd_words.txt", "r") do |f|
+    f.each_line do |word|
+      words.add(word.downcase.strip)
+    end
+  end
+  File.open("human_words.txt", "r") do |f|
+    f.each_line do |word|
+      words.add(word.strip)
+    end
+  end
+  words = words.to_a
+  for n in (1..5) do
+    (0...TEST_PER_QUERY).each do
+      query = []
+      rand_n(n, words.size).each do |i|
+        query << words[i]
+      end
+      query = query.join('+')
+      puts query
+    end
+  end
+
+end
+
+benchmark_random_queries
